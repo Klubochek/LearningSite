005a4e70ac3decc9c54efd285f1e3b9b12d98189
Vladislav Kochkin <46074880+Klubochek@users.noreply.github.com>
2023-01-30 23:42:56 +0200

    30.01.2023 23:43

---
+++ Controllers/CoursesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Learning_Site.Controllers
+{
+    public class CoursesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
